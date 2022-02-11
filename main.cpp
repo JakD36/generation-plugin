430b76c2bc4086bd437c398deb9ae5d59ae63ab5
Jack Davidson <jack.davidson@tag-games.com>
2022-02-11 14:20:50 +0000

    Changed the functionality to load directly from the dylib.
    Added ability to hot reload the dll, seems to be as easy as just using dlclose and dlopen.

--- main.cpp
+++ main.cpp
@@ -2,21 +2,65 @@
 // Created by Jack on 18/12/2021.
 //
 
-#include "src/library.h"
+// #include <stdlib.h>
+#include <dlfcn.h>
 #include <iostream>
+#include "glm/glm.hpp"
+#include <tuple>
+#include <vector>
 
 int main()
 {
-
     glm::vec4 regionRect = glm::vec4(0,0,10,10);
     int size = 0;
 
-    glm::vec2* arr = PoissonDiscSampling(1.f, regionRect, 30, size);
+    void* handle;
+    char* error;
+    glm::vec2* (*PoissonDiscSampling)(float, glm::vec4, int, int&);
 
-    for (int i = 0; i < size; ++i)
+
+    handle = dlopen("libgeneration-plugin.dylib", RTLD_LOCAL);
+    if(!handle)
     {
-        std::cout << arr[i].x << " " << arr[i].y << std::endl;
+        std::cout << "Failed to load dylib" << std::endl;
+        exit(2);
     }
 
-    std::cout << size << std::endl;
+    *(void **) (&PoissonDiscSampling) = dlsym(handle, "PoissonDiscSampling");
+    
+    while(true)
+    {
+        std::string s;
+        std::cin >> s;
+        if(s == "close")
+        {
+            break;
+        }
+        else if (s == "reload")
+        {
+            std::cout << "Attempting to Reload dylib" << std::endl;
+            dlclose(handle);
+
+            handle = dlopen("libgeneration-plugin.dylib", RTLD_LOCAL);
+            if(!handle)
+            {
+                std::cout << "Failed to load dylib" << std::endl;
+                exit(2);
+            }
+            *(void **) (&PoissonDiscSampling) = dlsym(handle, "PoissonDiscSampling");
+        }
+        else if(s == "run")
+        {
+            glm::vec2* arr = PoissonDiscSampling(1.f, regionRect, 30, size);
+            for (int i = 0; i < size; ++i)
+            {
+                std::cout << arr[i].x << " " << arr[i].y << std::endl;
+            }
+            std::cout << size << std::endl;
+            delete arr;
+        }
+    }
+    
+
+    dlclose(handle);
 }
